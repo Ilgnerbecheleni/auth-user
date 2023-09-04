e1321174930c5de022d271afce188d6fc1922f23
ilgnerbecheleni <ilgner.gui@gmail.com>
2023-09-04 20:27:44 -0300

    view principal e login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ImagemController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/imagem/{nomeDaImagem}', 'ImagemController@mostrarImagem($nomeDaImagem)');
+Route::get('/login', function () {
+    return view('login');
+});
 
